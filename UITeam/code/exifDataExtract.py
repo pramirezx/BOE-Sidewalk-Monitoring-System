e98306d5d66f628ca7495e95dbdda1928fed7318
Francisco Gastelum <franciscogastelum18@gmail.com>
2021-11-20 16:35:34 -0800

    Updated exif data code
    Uploaded all images onto the azure blob server. Then updated the exifDataExtract.py file to now be able to look through all images in the blob and extract the metadata from each image. The code now creates a csv file on the local machine and saves the metadata from each image on the csv.

--- UITeam/code/exifDataExtract.py
+++ UITeam/code/exifDataExtract.py
@@ -1,30 +1,67 @@
-# Getting the EXIF metadata using the EXIF Python library
-from exif import Image
+from azure.storage.blob import BlockBlobService
+from PIL import Image
+import datetime
+import exifread
+import csv
+import io
 
-img_path = './code/GPBK1903.jpg'
-with open(img_path, 'rb') as src:
-    img = Image(src)
-    print (src.name, img)
+# Get Azure Blob Server Information
+block_blob_service = BlockBlobService(account_name='swbotblob', sas_token='sv=2019-10-10&si=csulaswproject-17C19A6298B&sr=c&sig=xZ2soDIU1tHpJVeQVKnAYykG32Sggh3C5xNTnv1%2FB6Y%3D')
+generator = block_blob_service.list_blobs('csulaswproject', 'images')
 
+# Convert exif data to degree
+def toDegree(data):
+        l = list((str(data).replace('[', '')).replace(']', '').split(','))
+        s = l[2].split('/')
+        return float(l[0]) + (1/60)*float(l[1]) + (float(s[0])/float(s[1]))*(1/(60*60))
 
-# Open image
-import PIL
-from PIL import Image as PILImage
+# Open or create csv file
+f = open('./data/GoProMetadata.csv', 'w', encoding='UTF8', newline='')
 
-image = PILImage.open(img_path)
-image
+# Create a wirter to write onto the csv file
+writer = csv.writer(f)
 
+# Write the header onto the csv file
+header = ['image_name','date','time','latitude','latref','longitude','lonref','lat_degrees','lat_minutes','lat_seconds','lon_degrees','lon_minutes','lon_seconds']
+writer.writerow(header)
 
-# Checking out if there is any exif metadata available for the picture
-if img.has_exif:
-    info = f" has the EXIF {img.exif_version}"
-else:
-    info = "does not contain any EXIF information"
-print(f"Image {src.name}: {info}")
+# Loops through all images located in the blob server directory
+for blob in generator:
+    # Open current image in the directory
+    b = block_blob_service.get_blob_to_bytes('csulaswproject', blob.name)
+    image_file_in_mem = io.BytesIO(b.content)
 
+    # Reads all metadata located within the exif file
+    tags = exifread.process_file(image_file_in_mem)
 
-# List all tags available on the image
-with open(img_path, "rb") as src:
-    img = Image(src)
-print(img.list_all())
-# print(img.pixel_x_dimension)
+    # Extract image name
+    img_name = b.name.split('/')[-1]
+
+    # Extract image gps data
+    latref = tags.get('GPS GPSLatitudeRef')
+    lat = toDegree(tags.get('GPS GPSLatitude'))
+    lonref = tags.get('GPS GPSLongitudeRef')
+    lon = toDegree(tags.get('GPS GPSLongitude'))
+    imake = tags.get('Image Make')
+
+    # Extract date and time data from the current exif image file
+    imageTime = datetime.datetime.strptime(str(tags.get('Image DateTime')), '%Y:%m:%d %H:%M:%S')
+    date, time = str(imageTime).split(' ') 
+
+    # Extract and convert Degree, Minute and Second of the exif image file
+    lat_deg, lat_min, lat_sec = str(tags.get('GPS GPSLatitude'))[1:-1].split(', ')
+    lat_sec_num1, lat_sec_num2 = lat_sec.split('/')
+    lat_sec = float(lat_sec_num1)/float(lat_sec_num2)
+
+    lon_deg, lon_min, lon_sec = str(tags.get('GPS GPSLongitude'))[1:-1].split(', ')
+    lon_sec_num1, lon_sec_num2 = lon_sec.split('/')
+    lon_sec = float(lon_sec_num1)/float(lon_sec_num2)
+
+    # Group all metadata into a list
+    data = [img_name,date,time,lat,latref,lon,lonref,lat_deg,lat_min,lat_sec,lon_deg,lon_min,lon_sec]
+
+    # Write the data onto the file
+    writer.writerow(data)
+
+# Close the file
+f.close()
