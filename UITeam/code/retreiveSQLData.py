3c054c949d4e5f94c2e8b87113149be202e66576
Francisco Gastelum <franciscogastelum18@gmail.com>
2021-12-08 21:16:11 -0800

    Updated Database table
    The table that the data will be extracted from has been updated to the latest/correct version

--- UITeam/code/retreiveSQLData.py
+++ UITeam/code/retreiveSQLData.py
@@ -1,6 +1,9 @@
 import pyodbc
 import pandas as pd
 
+def handle_sql_variant_as_string(value):
+    return value.decode('ISO-8859-1')
+
 # Use SELECT @@SERVERNAME to check server name on SQL Server
 con = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
                         "Server=engswbotdbserver.database.windows.net;"
@@ -9,18 +12,27 @@ con = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
                     )
 
 cursor = con.cursor()
-cursor.execute('SELECT * FROM [dbo].[RoverData]')
+con.add_output_converter(-151, handle_sql_variant_as_string)
+cursor.execute('SELECT [ID]'
+      ',[INPUT_SW_ID]'
+      ',[RAW_GPS]'
+      ',[LATITUDE]'
+      ',[LONGITUDE]'
+      ',[DATE]'
+      ',[SLOPE_X]'
+      ',[SLOPE_Y]'
+      ',[RECORD_FILE]'
+      ',[SRID] FROM [sdwk].[rover_data]')
 
 for row in cursor:
-    rawGPS = row[0]
-    timeStamps = row[1]
-    latitude = row[2]
-    latitudeREF = row[3]
+    id = row[0]
+    input_sw_id = row[1]
+    raw_gps = row[2]
+    latitude = row[3]
     longitude = row[4]
-    longitudeREF = row[5]
-    xSlope = row[6]
-    ySlope = row[7]
-    angle = row[8]
-    pinID = row[9]
-    imageURL = row[10]
-    print('row = %r' % (row,))
+    date = row[5]
+    slope_x = row[6]
+    slope_y = row[7]
+    record_file = row[8]
+    srid = row[9]
+    # print('row = %r' % (row,))
